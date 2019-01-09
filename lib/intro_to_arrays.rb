3872223171e3ffe2db85e68bdf8ba654a0801eb8
Samidh Patel <spatel13456@gmail.com>
2019-01-09 03:28:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,26 @@
+def instantiate_new_array
+  array = Array.new
+  return array
+end
+
+def array_with_two_elements
+  array = [1, 2]
+  return array
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array[-1]
+end
+
+def first_element_with_array_methods(array)
+end
+  
+  
